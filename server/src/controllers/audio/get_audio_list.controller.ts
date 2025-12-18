59fe9ddbbb072ce56232b5e99fb89373dcb4ea4a
Danial-03-dev <danialzafarwork003@gmail.com>
2025-12-18 17:04:13 +0500

    feat: add route to get list of audios

---
+++ server/src/controllers/audio/get_audio_list.controller.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from "express";
+import { AudioModel } from "../../models/audio.model";
+
+export const getAudioListController = async (_req: Request, res: Response) => {
+    try {
+        const audioList = await AudioModel.find();
+        return res.status(200).json({ audioList });
+    } catch (error) {
+        return res.status(500).json({ message: "Internal server error" });
+    }
+};
--- server/src/routes/audio.routes.ts
+++ server/src/routes/audio.routes.ts
@@ -2,8 +2,10 @@ import { Router } from "express";
 import { uploadAudioController } from "../controllers/audio/upload_audio.controller";
 import { uploadAudioAndImageMiddleware } from "../middleware/audio.middleware";
 import { authMiddleware } from "../middleware/auth.middleware";
+import { getAudioListController } from "../controllers/audio/get_audio_list.controller";
 
 export const audioRouter = Router();
 
 
+audioRouter.get('/list', authMiddleware, getAudioListController);
 audioRouter.post('/upload', authMiddleware, uploadAudioAndImageMiddleware, uploadAudioController);
