1de80876cf55958eca77aa3e9cc50d75e5c58bfc
Danial-03-dev <danialzafarwork003@gmail.com>
2026-01-25 16:18:57 +0500

    feat: add production url

--- client/lib/core/constants/server_constants.dart
+++ client/lib/core/constants/server_constants.dart
@@ -1,5 +1,9 @@
-class ServerConstants {
-  static const String _localIP = '10.25.0.119';
+import 'package:flutter/foundation.dart';
 
-  static String serverURL = 'http://$_localIP:3000';
+class ServerConstants {
+  static const String _localIP = '172.27.64.119';
+
+  static String serverURL = kReleaseMode
+      ? 'https://music-app-delta-wheat.vercel.app'
+      : 'http://$_localIP:3000';
 }
